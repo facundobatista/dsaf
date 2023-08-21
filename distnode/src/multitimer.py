26641cb77d297f57234c0303e78c98752e1bbb61
Facundo Batista <facundo@taniquetil.com.ar>
2023-08-21 10:17:04 -0300

    Simplified multitimer usage.

--- distnode/src/multitimer.py
+++ distnode/src/multitimer.py
@@ -4,22 +4,39 @@
 """A multi-timer based on a unique hardware timer."""
 
 
-def _timer_id_generator():
+# tick in milliseconds
+TICK_DELAY = 100
+
+# modes of working
+ONE_SHOT = "oneshot"
+PERIODIC = "periodic"
+_mode_periodic = {
+    ONE_SHOT: False,
+    PERIODIC: True,
+}
+
+# collection of created timers
+_timers = {}
+
+
+def _ints_generator():
+    """Infinite generator of integers."""
     value = 0
     while True:
         yield value
         value += 1
 
 
-_get_timer_id = _timer_id_generator()
-
-
 class _Timer:
+    """A single timer."""
+
+    _get_timer_id = _ints_generator()
+
     def __init__(self, is_periodic, ticks_period, callback):
         self.is_periodic = is_periodic
         self.ticks_period = self.remaining_ticks = ticks_period
         self.callback = callback
-        self.timer_id = next(_get_timer_id)
+        self.timer_id = next(self._get_timer_id)
         self.expired = False
 
     def tick(self):
@@ -44,51 +61,37 @@ class _Timer:
         self.remaining_ticks = self.ticks_period
 
 
-class MultiTimer:  # FIXME: not a class
+def init(period, mode, callback):
+    """Initiates a timer.
 
-    # tick in milliseconds
-    TICK_DELAY = 100
+    Period is in milliseconds and must be a multiple of TICK_DELAY.
+    """
+    tick_period, period_rest = divmod(period, TICK_DELAY)
+    if period <= 0 or period_rest:
+        raise ValueError("Period must be a positive multiple of TICK_DELAY")
+    try:
+        is_periodic = _mode_periodic[mode]
+    except KeyError:
+        raise ValueError("Mode must be ONE_SHOT or PERIODIC")
 
-    # modes of working
-    ONE_SHOT = "oneshot"
-    PERIODIC = "periodic"
-    _mode_periodic = {
-        ONE_SHOT: False,
-        PERIODIC: True,
-    }
+    new_timer = _Timer(is_periodic, tick_period, callback)
+    _timers[new_timer.timer_id] = new_timer
+    return new_timer.timer_id
 
-    def __init__(self):
-        self._timers = {}
 
-    def init(self, period, mode, callback):
-        """Initiates a timer.
+def deinit(timer_id):
+    """Deactivate/disable/remove a timer."""
+    oldtimer = _timers.pop(timer_id, None)
+    if oldtimer is None:
+        raise ValueError(f"Tried to deinit a missing timer: {timer_id!r}")
 
-        Period is in milliseconds and must be a multiple of TICK_DELAY.
-        """
-        tick_period, period_rest = divmod(period, self.TICK_DELAY)
-        if period <= 0 or period_rest:
-            raise ValueError("Period must be a positive multiple of TICK_DELAY")
-        try:
-            is_periodic = self._mode_periodic[mode]
-        except KeyError:
-            raise ValueError("Mode must be ONE_SHOT or PERIODIC")
 
-        new_timer = _Timer(is_periodic, tick_period, callback)
-        self._timers[new_timer.timer_id] = new_timer
-        return new_timer.timer_id
+def tick():
+    """Move the machinery.
 
-    def deinit(self, timer_id):
-        """Deactivate/disable/remove a timer."""
-        oldtimer = self._timers.pop(timer_id, None)
-        if oldtimer is None:
-            raise ValueError(f"Tried to deinit a missing timer: {timer_id!r}")
-
-    def tick(self):
-        """Move the machinery.
-
-        It must be called externally every TICK_DELAY (normally hooked into the
-        microcontroller's hardware timer).
-        """
-        for timer in list(self._timers.values()):
-            if not timer.expired:
-                timer.tick()
+    It must be called externally every TICK_DELAY (normally hooked into the
+    microcontroller's hardware timer).
+    """
+    for timer in list(_timers.values()):
+        if not timer.expired:
+            timer.tick()
--- distnode/tests/test_multitimer.py
+++ distnode/tests/test_multitimer.py
@@ -5,7 +5,7 @@
 
 import pytest
 
-from src.multitimer import MultiTimer
+from src import multitimer
 
 
 class _CallCounter:
@@ -25,255 +25,239 @@ class _CallCounter:
 
 @pytest.fixture
 def call_counter():
+    """Provide a _CallCounter instance."""
     return _CallCounter()
+
+
+@pytest.fixture(autouse=True)
+def timer_cleaner():
+    """Automatically clean timers from any run test."""
+    multitimer._timers.clear()
 
 
 def test_simple_oneshot_fast(call_counter):
     """Only one timer, one call, next one."""
-    mt = MultiTimer()
-
-    period = MultiTimer.TICK_DELAY
-    mt.init(period=period, mode=MultiTimer.ONE_SHOT, callback=call_counter)
+    period = multitimer.TICK_DELAY
+    multitimer.init(period=period, mode=multitimer.ONE_SHOT, callback=call_counter)
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 1
     for _ in range(10):
-        mt.tick()
+        multitimer.tick()
     assert call_counter.total == 1
 
 
 def test_simple_oneshot_later(call_counter):
     """Only one timer, one call, later."""
-    mt = MultiTimer()
-
-    period = MultiTimer.TICK_DELAY * 3
-    mt.init(period=period, mode=MultiTimer.ONE_SHOT, callback=call_counter)
+    period = multitimer.TICK_DELAY * 3
+    multitimer.init(period=period, mode=multitimer.ONE_SHOT, callback=call_counter)
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 1
     for _ in range(10):
-        mt.tick()
+        multitimer.tick()
     assert call_counter.total == 1
 
 
 def test_simple_periodic(call_counter):
     """Only one timer, periodic."""
-    mt = MultiTimer()
-
-    period = MultiTimer.TICK_DELAY * 3
-    mt.init(period=period, mode=MultiTimer.PERIODIC, callback=call_counter)
+    period = multitimer.TICK_DELAY * 3
+    multitimer.init(period=period, mode=multitimer.PERIODIC, callback=call_counter)
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 1
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 1
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 1
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 2
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 2
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 2
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 3
 
 
 def test_deinit_oneshot_before(call_counter):
     """Disable a timer before getting called."""
-    mt = MultiTimer()
-
-    period = MultiTimer.TICK_DELAY * 3
-    timer_id = mt.init(period=period, mode=MultiTimer.ONE_SHOT, callback=call_counter)
+    period = multitimer.TICK_DELAY * 3
+    timer_id = multitimer.init(period=period, mode=multitimer.ONE_SHOT, callback=call_counter)
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 0
 
-    mt.deinit(timer_id)
+    multitimer.deinit(timer_id)
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 0
     for _ in range(10):
-        mt.tick()
+        multitimer.tick()
     assert call_counter.total == 0
 
 
 def test_deinit_oneshot_after(call_counter):
     """Disable a one shot timer after being called (should be a no-op)."""
-    mt = MultiTimer()
-
-    period = MultiTimer.TICK_DELAY * 2
-    timer_id = mt.init(period=period, mode=MultiTimer.ONE_SHOT, callback=call_counter)
+    period = multitimer.TICK_DELAY * 2
+    timer_id = multitimer.init(period=period, mode=multitimer.ONE_SHOT, callback=call_counter)
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 1
 
-    mt.deinit(timer_id)
+    multitimer.deinit(timer_id)
 
     for _ in range(10):
-        mt.tick()
+        multitimer.tick()
     assert call_counter.total == 1
 
 
 def test_deinit_periodic(call_counter):
     """Disable a periodic timer."""
-    mt = MultiTimer()
-
-    period = MultiTimer.TICK_DELAY * 2
-    timer_id = mt.init(period=period, mode=MultiTimer.PERIODIC, callback=call_counter)
+    period = multitimer.TICK_DELAY * 2
+    timer_id = multitimer.init(period=period, mode=multitimer.PERIODIC, callback=call_counter)
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 0
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 1
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 1
 
-    mt.deinit(timer_id)
+    multitimer.deinit(timer_id)
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.total == 1
     for _ in range(10):
-        mt.tick()
+        multitimer.tick()
     assert call_counter.total == 1
 
 
 def test_multiple_two_oneshots(call_counter):
     """Two one shots."""
-    mt = MultiTimer()
-
-    period = MultiTimer.TICK_DELAY * 2
-    timer_id_1 = mt.init(period=period, mode=MultiTimer.ONE_SHOT, callback=call_counter)
-    period = MultiTimer.TICK_DELAY * 5
-    timer_id_2 = mt.init(period=period, mode=MultiTimer.ONE_SHOT, callback=call_counter)
+    period = multitimer.TICK_DELAY * 2
+    timer_id_1 = multitimer.init(period=period, mode=multitimer.ONE_SHOT, callback=call_counter)
+    period = multitimer.TICK_DELAY * 5
+    timer_id_2 = multitimer.init(period=period, mode=multitimer.ONE_SHOT, callback=call_counter)
 
     assert call_counter.calls == []
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == []
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1]
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1]
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1]
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_2]
 
     for _ in range(10):
-        mt.tick()
+        multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_2]
 
 
 def test_multiple_two_periodics(call_counter):
     """Two periodics."""
-    mt = MultiTimer()
-
-    period = MultiTimer.TICK_DELAY * 2
-    timer_id_1 = mt.init(period=period, mode=MultiTimer.PERIODIC, callback=call_counter)
-    period = MultiTimer.TICK_DELAY * 5
-    timer_id_2 = mt.init(period=period, mode=MultiTimer.PERIODIC, callback=call_counter)
+    period = multitimer.TICK_DELAY * 2
+    timer_id_1 = multitimer.init(period=period, mode=multitimer.PERIODIC, callback=call_counter)
+    period = multitimer.TICK_DELAY * 5
+    timer_id_2 = multitimer.init(period=period, mode=multitimer.PERIODIC, callback=call_counter)
     assert call_counter.calls == []
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == []
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1]
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1]
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_1]
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_1, timer_id_2]
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_1, timer_id_2, timer_id_1]
 
 
 def test_multiple_mixed_with_deinit(call_counter):
     """Diverse combination."""
-    mt = MultiTimer()
-
-    period = MultiTimer.TICK_DELAY * 2
-    timer_id_1 = mt.init(period=period, mode=MultiTimer.PERIODIC, callback=call_counter)
-    period = MultiTimer.TICK_DELAY * 3
-    timer_id_2 = mt.init(period=period, mode=MultiTimer.ONE_SHOT, callback=call_counter)
-    period = MultiTimer.TICK_DELAY * 5
-    timer_id_3 = mt.init(period=period, mode=MultiTimer.ONE_SHOT, callback=call_counter)
+    period = multitimer.TICK_DELAY * 2
+    timer_id_1 = multitimer.init(period=period, mode=multitimer.PERIODIC, callback=call_counter)
+    period = multitimer.TICK_DELAY * 3
+    timer_id_2 = multitimer.init(period=period, mode=multitimer.ONE_SHOT, callback=call_counter)
+    period = multitimer.TICK_DELAY * 5
+    timer_id_3 = multitimer.init(period=period, mode=multitimer.ONE_SHOT, callback=call_counter)
     assert call_counter.calls == []
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == []
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1]
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_2]
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_2, timer_id_1]
 
-    mt.tick()
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_2, timer_id_1, timer_id_3]
-    mt.deinit(timer_id_1)
-    mt.tick()
+    multitimer.deinit(timer_id_1)
+    multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_2, timer_id_1, timer_id_3]
 
     for _ in range(10):
-        mt.tick()
+        multitimer.tick()
     assert call_counter.calls == [timer_id_1, timer_id_2, timer_id_1, timer_id_3]
 
 
 @pytest.mark.parametrize("period", [123.15, 0, -3])
 def test_init_bad_period(period):
     """Only one timer, one call, next one."""
-    mt = MultiTimer()
     with pytest.raises(ValueError):
-        mt.init(period=period, mode=MultiTimer.ONE_SHOT, callback=None)
+        multitimer.init(period=period, mode=multitimer.ONE_SHOT, callback=None)
 
 
 def test_init_bad_mode():
     """Only one timer, one call, next one."""
-    mt = MultiTimer()
     with pytest.raises(ValueError):
-        mt.init(period=MultiTimer.TICK_DELAY, mode="never", callback=None)
+        multitimer.init(period=multitimer.TICK_DELAY, mode="never", callback=None)
 
 
 def test_deinit_missing_timer_id():
     """Only one timer, one call, next one."""
-    mt = MultiTimer()
     with pytest.raises(ValueError):
-        mt.deinit(-1)
+        multitimer.deinit(-1)
 
 
 def test_deinit_twice():
     """Only one timer, one call, next one."""
-    mt = MultiTimer()
-    timer_id = mt.init(period=MultiTimer.TICK_DELAY, mode=MultiTimer.ONE_SHOT, callback=None)
-    mt.deinit(timer_id)  # this first one should be ok
+    timer_id = multitimer.init(
+        period=multitimer.TICK_DELAY, mode=multitimer.ONE_SHOT, callback=None)
+    multitimer.deinit(timer_id)  # this first one should be ok
     with pytest.raises(ValueError):
-        mt.deinit(timer_id)
+        multitimer.deinit(timer_id)
 
 
 def test_callback_robustness(capsys):
     """Callback should always be called, even if fails sometimes."""
-    mt = MultiTimer()
-
     calls_record = 0
 
     def the_callback(_):
@@ -282,19 +266,22 @@ def test_callback_robustness(capsys):
         if calls_record == 3:
             raise ValueError("oops")
 
-    mt.init(period=MultiTimer.TICK_DELAY, mode=MultiTimer.PERIODIC, callback=the_callback)
+    timer_id = multitimer.init(
+        period=multitimer.TICK_DELAY, mode=multitimer.PERIODIC, callback=the_callback)
     assert calls_record == 0
-    mt.tick()
+    multitimer.tick()
     assert calls_record == 1
-    mt.tick()
+    multitimer.tick()
     assert calls_record == 2
 
     # this one will explode
-    mt.tick()
+    multitimer.tick()
     captured = capsys.readouterr()
-    assert captured.out == "Error when calling callback from timer 0: ValueError('oops')\n"
+    assert captured.out == (
+        f"Error when calling callback from timer {timer_id}: ValueError('oops')\n"
+    )
     assert calls_record == 3
 
     # life just goes on
-    mt.tick()
+    multitimer.tick()
     assert calls_record == 4
